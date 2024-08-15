ee57556c0711e8bf490b2572027b4f1ca0159373
Rauf Salamzade <salamzader@gmail.com>
2024-08-14 22:46:55 -0500

    Update test_docker.sh

--- docker/test_docker.sh
+++ docker/test_docker.sh
@@ -19,4 +19,4 @@ cd test_case/
 ln -s ../run_lsaBGC-Pan.sh .
 
 # run test
-./run_lsaBGC-Pan.sh -i input_genomes/ -o lsabgc_pan_results/ -nb
+./run_lsaBGC-Pan.sh -i input_genomes/ -o lsabgc_pan_results/ -nb -c 4
